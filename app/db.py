28882c0c6538d0a1f8e8e3f485e4657dfb01c2b6
RA <permale.ra.tech@gmail.com>
2025-06-27 02:24:21 +1000

    updated models for further feature

--- app/db.py
+++ app/db.py
@@ -1,8 +1,8 @@
 from pymongo import MongoClient
 from werkzeug.security import generate_password_hash
 
-from .models import User
-
 client = MongoClient("mongodb+srv://GreyTech:Kronos12@chatapp.2ebkk3a.mongodb.net/")
 chat_db = client.get_database("ChatDB")
 users_collection = chat_db.get_collection("users")
+rooms_collection = chat_db.get_collection("rooms")
+room_members_collection = chat_db.get_collection("room_members")
--- app/models.py
+++ app/models.py
@@ -1,38 +1,96 @@
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import datetime
+from .db import rooms_collection, users_collection, room_members_collection
+from bson.objectid import ObjectId
+
 
 class User:
-  def __init__(self, username, email, password):
-    self.username = username
-    self.email = email
-    self.password = password
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
 
-  @staticmethod
-  def is_authenticated(self):
-    return True
+    @staticmethod
+    def is_authenticated(self):
+        return True
 
-  @staticmethod
-  def is_active(self):
-    return True
+    @staticmethod
+    def is_active(self):
+        return True
 
-  @staticmethod
-  def is_anonymous(self):
-    return False
+    @staticmethod
+    def is_anonymous(self):
+        return False
 
-  def get_id(self):
-    return self.username
-  
-  def check_password(self, password_input):
-    return check_password_hash(self.password, password_input)
+    def get_id(self):
+        return self.username
 
-  @staticmethod
-  def save_user(username, email, password):
-    from werkzeug.security import generate_password_hash
-    from .db import users_collection
-    password_hash = generate_password_hash(password)
-    users_collection.insert_one({'_id': username, 'email': email, 'password': password_hash})
+    def check_password(self, password_input):
+        return check_password_hash(self.password, password_input)
 
-  @staticmethod
-  def get_user(username):
-    from .db import users_collection
-    user_data = users_collection.find_one({'_id': username})
-    return User(user_data['_id'], user_data['email'], user_data['password']) if user_data else None
+    @staticmethod
+    def save_user(username, email, password):
+        password_hash = generate_password_hash(password)
+        users_collection.insert_one(
+            {"_id": username, "email": email, "password": password_hash}
+        )
+
+    @staticmethod
+    def get_user(username):
+        user_data = users_collection.find_one({"_id": username})
+        return (
+            User(user_data["_id"], user_data["email"], user_data["password"])
+            if user_data
+            else None
+        )
+
+
+class Room:
+    def __init__(self, room_id, room_name):
+        self.room_id = room_id
+        self.room_nmae = room_name
+
+    @staticmethod
+    def save_room(room_name, created_by):
+        room_id = rooms_collection.insert_one(
+            {
+                "room_name": room_name,
+                "created_by": created_by,
+                "created_at": datetime.now(),
+            }
+        ).inserted_id
+        Room.add_room_member(
+            room_id, room_name, created_by, created_by, is_room_admin=True
+        )
+        return room_id
+
+    @staticmethod
+    def get_room(room_id):
+        rooms_collection.find_one({"_id": ObjectId(room_id)})
+
+    @staticmethod
+    def add_room_member(room_id, room_name, username, added_by, is_room_admin=False):
+        room_members_collection.insert_one(
+            {
+                "_id": {"room_id": room_id, "username": username},
+                "room_name": room_name,
+                "added_by": added_by,
+                "added_at": datetime.now(),
+                "is_room_admin": is_room_admin,
+            }
+        )
+
+    @staticmethod
+    def add_room_members(room_id, room_name, usernames, added_by):
+        room_members_collection.insert_many(
+            [
+                {
+                    "_id": {"room_id": room_id, "username": username},
+                    "room_name": room_name,
+                    "added_by": added_by,
+                    "added_at": datetime.now(),
+                    "is_room_admin": False,
+                }
+                for username in usernames
+            ]
+        )
